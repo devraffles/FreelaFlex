cf7f82c7e9e9c7267b61b19100abc0fc5b2c4007
Rafael dos Santos Giorgi <106035150+devraffles@users.noreply.github.com>
2024-11-25 11:11:29 -0300

    Not Found

--- src/components/boxMyProjects/boxMyProjects.jsx
+++ src/components/boxMyProjects/boxMyProjects.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import UrlAxios from '../../urlaxios/axios';
+import NotFound from '../../pages/notfound/notfound';
 
 
 export default function BoxMyProjects({page, dados, DeleteProject,DeletaProposta,id}) {
@@ -26,6 +27,7 @@ export default function BoxMyProjects({page, dados, DeleteProject,DeletaProposta
         handlePropostaProjeto()
         
     }, [setProposta, id]);
+
     return (
         <section className="section-projeto-form" data-aos="fade-right" data-aos-duration="2000">
             <div className="row-projeto-form">
@@ -86,70 +88,76 @@ export default function BoxMyProjects({page, dados, DeleteProject,DeletaProposta
                     </div>
                 ) : (
                     <>
-                    {proposta.map((item) =>(
-                        <>
+                        {proposta.length === 0 ? (
+                            <NotFound/>
+                        ) : (
+                            <>
+                                {proposta.map((item) =>(
+                                    <>
 
-                        <div className="row-projeto-form">
-                        <div className="descricao-div">
-                                <h1>Detalhes:</h1>
-                                <p>{item?.descricao}</p>
-                            </div>
-                        </div>
-                        <div className="row-projeto-form">
-                            <div className="habilidades-div">
-                                <h1>Dados Pessoais:</h1>
-                                <p>{item.nome}</p>
-                                <p>{item.telefone}</p>
-                                <p>{item.email}</p>
-                            </div>
-                        </div>
-                        <div className='content-myproposal'>  
-                            <div className="card">
-                                <h2 className="title-cargo">Cargo</h2>
-                                <div className="input-cargos">
-                                    <input className="input-cargo" placeholder={item.nomecargo} type="text" name="cargo" readOnly />
-                                </div>
-                            </div>
-                                
-                            <div className="card">
-                                <h2 className="title-card">Sua Oferta</h2>
-                                <div className="input-cards">
-                                    <h3 className="tipo-input-card">R$</h3>
-                                    <input
-                                        className="input-card"
-                                        type="number"
-                                        name="price"
-                                        required
-                                        placeholder={item.oferta}
-                                    />
-                                </div>
-                            </div>
-                            
-                            <div className="card">
-                                <h2 className="title-card">Duração estimada</h2>
-                                <div className="input-cards">
-                                    <h3 className="tipo-input-card">Dias</h3>
-                                    <input
-                                        className="input-card"
-                                        type="text"
-                                        name="data"
-                                        required
-                                        placeholder={item.duracao_estimada}
-                                        readOnly
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row-projeto-form">
-                            <div className='box-button-proposal'>
-                                
-                                    <button className='button-proposal' onClick={() =>DeletaProposta(item.codproposta)}>Aceitar</button>
-                            
-                            </div>
-                        </div>
-                        </>
-                        ))}
-                        </>
+                                    <div className="row-projeto-form">
+                                    <div className="descricao-div">
+                                            <h1>Detalhes:</h1>
+                                            <p>{item?.descricao}</p>
+                                        </div>
+                                    </div>
+                                    <div className="row-projeto-form">
+                                        <div className="habilidades-div">
+                                            <h1>Dados Pessoais:</h1>
+                                            <p>{item.nome}</p>
+                                            <p>{item.telefone}</p>
+                                            <p>{item.email}</p>
+                                        </div>
+                                    </div>
+                                    <div className='content-myproposal'>  
+                                        <div className="card">
+                                            <h2 className="title-cargo">Cargo</h2>
+                                            <div className="input-cargos">
+                                                <input className="input-cargo" placeholder={item.nomecargo} type="text" name="cargo" readOnly />
+                                            </div>
+                                        </div>
+                                            
+                                        <div className="card">
+                                            <h2 className="title-card">Sua Oferta</h2>
+                                            <div className="input-cards">
+                                                <h3 className="tipo-input-card">R$</h3>
+                                                <input
+                                                    className="input-card"
+                                                    type="number"
+                                                    name="price"
+                                                    required
+                                                    placeholder={item.oferta}
+                                                />
+                                            </div>
+                                        </div>
+                                        
+                                        <div className="card">
+                                            <h2 className="title-card">Duração estimada</h2>
+                                            <div className="input-cards">
+                                                <h3 className="tipo-input-card">Dias</h3>
+                                                <input
+                                                    className="input-card"
+                                                    type="text"
+                                                    name="data"
+                                                    required
+                                                    placeholder={item.duracao_estimada}
+                                                    readOnly
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row-projeto-form">
+                                        <div className='box-button-proposal'>
+                                            
+                                                <button className='button-proposal' onClick={() =>DeletaProposta(item.codproposta)}>Aceitar</button>
+                                        
+                                        </div>
+                                    </div>
+                                    </>
+                                ))}
+                            </>
+                        )}
+                    </>
                 )}
             </div>
         </section>
